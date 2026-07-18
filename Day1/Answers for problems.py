5acbd1f7ee974391e559937563ac29c2ff9e0ac2
Sowdhanya Vijayakumar <125268010+Sowdhanya-laxna@users.noreply.github.com>
2026-07-18 22:36:56 +0530

    Add files via upload

---
+++ Day1/Answers for problems.py
@@ -0,0 +1,129 @@
+"""
+Question:01
+
+print("Welcome to python")
+print("I am learning programming")
+
+Question:02
+
+a = input("Enter the name: ")
+b = int(input("Enter the age: "))
+c = input("Enter the College name: ")
+d = float(input("Enter your CGPA: "))
+
+print("My name is", a, "I am", b, "years old, Student of", c, "CGPA", d)
+
+Question:03
+
+student_name = "Sowdhanya"
+roll_number = 101
+percentage = 84.79
+is_graduated = True
+
+print(student_name)
+print(type(student_name))
+
+print(roll_number)
+print(type(roll_number))
+
+print(percentage)
+print(type(percentage))
+
+print(is_graduated)
+print(type(is_graduated))
+
+Question:04
+
+a=int(input("Enter the a number: "))
+b=int(input("Enter the another number: "))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a//b)
+
+Question:05
+
+a = int(input("Enter your birth year: "))
+b = 2026
+age = b - a
+print("Your age is", age)
+
+Question:06
+
+a=int(input("Enter the radius:"))
+b=a*a
+print("Area of circle:",3.14*b)
+
+Question:07
+
+a=int(input("Enter the a number: "))
+b=int(input("Enter the a number: "))
+c=int(input("Enter the a number: "))
+d=int(input("Enter the a number: "))
+e=int(input("Enter the a number: "))
+T=a+b+c+d+e
+Av=T/5
+p=(T / 500) * 100
+
+print("Total:",T)
+print("Average:",Av)
+print("Percentage:",p)
+
+Question:08
+
+a=10
+b=20
+a,b=b,a
+print("a",a)
+print("b",b)
+
+Question:09
+
+num = input("Enter a number: ")
+
+integer_num = int(num)
+float_num = float(num)
+
+print(integer_num)
+print(type(integer_num))
+
+print(float_num)
+print(type(float_num))
+
+Question:10
+
+a=input("Enter the a Product: ")
+b=int(input("Enter the a Price: "))
+c=int(input("Enter the a Quantity:"))
+T=b*c
+
+print("\nPRODUCT:",a)
+print("TOTAL BILL:",T)
+
+Question:11
+
+a = input("Enter the name: ")
+b = int(input("Enter the age: "))
+c = float(input("Enter the Height: "))
+d = float(input("Enter your Weight: "))
+h = c * 0.3048
+bmi = d / (c * c)
+
+print("Name:", a)
+print("Age:", b)
+print("Your BMI is:", round(bmi, 2))
+
+
+Question:12
+
+c = float(input("Enter Celsius: "))
+
+print("Fahrenheit:", (c * 9/5) + 32, "°F")
+
+Question:13
+
+a=int(input("enter the amount:"))
+b=a/85
+print("Amount in USD:", round(b, 2), "$")
+
+"""
